587fb190da3e492aabf91bcab79ffc9d42cee956
josipIvatek <jivatek@tvz.hr>
2023-04-04 15:33:59 +0200

    PP2

--- main pp1.py
+++ main pp1.py
@@ -4,6 +4,16 @@ korisnici = []
 kategorije = []
 prodaje = []
 
+broj_osobnih = int(input('Unesite broj osobnih iskaznica: '))
+osobne_iskaznice = []
+for i in range(1,broj_osobnih+1):
+    osobna_iskaznica ={}
+    osobna_iskaznica['broj'] = int(input(f"Unesite broj {i}. osobne iskaznice: "))
+    osobna_iskaznica['oib'] = int(input(f"Unesite oib korisnika {i}. osobne iskaznice: "))
+    osobna_iskaznica['prebivaliste'] = (input(f"Unesite prebivaliste korisnika {i}. osobne iskaznice: "))
+    osobne_iskaznice.append(osobna_iskaznica)
+
+
 br_korisnika = int(input('Unesite broj korisnika: '))
 for i in range(1,br_korisnika+1):
     korisnik = {}
@@ -11,6 +21,12 @@ for i in range(1,br_korisnika+1):
     korisnik['prezime'] = input(f'Unesite prezime {i}. korisnika: ').capitalize()
     korisnik['telefon'] = int(input(f'Unesite telefon {i}. korisnika: '))
     korisnik['email'] = input(f'Unesite email {i}. korisnika: ').strip()
+
+    for j,osobna_iskaznica in enumerate(osobne_iskaznice, start=1):
+        print(f"\t{j}. {osobna_iskaznica['oib']} ")
+
+    odabrana_osobna=int(input('Odaberite osobnu iskaznicu: '))
+    korisnik['osobna']=osobne_iskaznice[odabrana_osobna-1]
     korisnici.append(korisnik)
 
 br_kategorija = int(input('Unesite broj kategorija: '))
@@ -65,6 +81,7 @@ for i,prodaja in enumerate(prodaje, start = 1):
     print(f"\tPrezime: {prodaja['korisnik']['prezime']}")
     print(f"\tTelefon: {prodaja['korisnik']['telefon']}")
     print(f"\tEmail: {prodaja['korisnik']['email']}")
+    print(f"\t OIB: {prodaja['korisnik']['osobna']['oib']}")
 
     print('Informacije o artiklu: ')
     print(f"\tNaslov: {prodaja['artikl']['naslov']}")
@@ -76,3 +93,5 @@ for i,prodaja in enumerate(prodaje, start = 1):
     print(f"\tMjesec: {prodaja['datum'].month}")
     print(f"\tGodina: {prodaja['datum'].year}")
     print('-'*20)
+
+
