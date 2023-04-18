2a4751ed2893514e7c53efe6b59d2b96005eda20
josipIvatek <jivatek@tvz.hr>
2023-04-18 12:19:23 +0200

    pp

--- kategorija/ispis_kategorije.py
+++ kategorija/ispis_kategorije.py
@@ -6,5 +6,5 @@ def get_kategorija(redni_broj, kategorija):
 def ispis_svih_kategorija(kategorije):
     for kategorija in kategorije:
         print(f"{kategorija['naziv']}:")
-        for kategorija['artikl'] in kategorija['artikli']:
-            ispis_artikla(kategorija['artikl'])
+        for artikl in kategorija['artikli']:
+            ispis_artikla(artikl)
